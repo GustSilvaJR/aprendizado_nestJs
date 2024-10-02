4a2201295551508f4bbc25a2903281758281d6e5
GustSilvaJR <gust.offic@gmail.com>
2024-10-01 23:44:15 -0300

    Creating entity for tags

--- src/courses/courses.module.ts
+++ src/courses/courses.module.ts
@@ -3,9 +3,10 @@ import { CoursesController } from './controllers/courses/courses.controller';
 import { CoursesService } from './services/courses/courses.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Course } from './entities/course.entity';
+import { Tags } from './entities/tags.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Course])],
+  imports: [TypeOrmModule.forFeature([Course, Tags])],
   controllers: [CoursesController],
   providers: [CoursesService],
 })
